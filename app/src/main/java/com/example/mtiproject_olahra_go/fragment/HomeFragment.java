bca32e0783bb360d5f91bedbddbdc63f8d1d6b8c
Andre Lay <lay.andre1334@gmail.com>
2022-01-11 15:56:22 +0700

    Card In home page

--- app/src/main/java/com/example/mtiproject_olahra_go/fragment/HomeFragment.java
+++ app/src/main/java/com/example/mtiproject_olahra_go/fragment/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.mtiproject_olahra_go.fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mtiproject_olahra_go.DBHelper;
 import com.example.mtiproject_olahra_go.LoginActivity;
@@ -23,6 +25,7 @@ public class HomeFragment extends Fragment {
     TextView tvGreet;
     SharedPreferences sp;
     UserDB userDB;
+    CardView cvFutsal, cvBadminton, cvBasket;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -42,13 +45,39 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        cvFutsal = view.findViewById(R.id.cardSoccer);
+        cvBasket = view.findViewById(R.id.cardBasket);
+        cvBadminton = view.findViewById(R.id.cardBadminton);
         tvGreet = view.findViewById(R.id.tvGreet);
+
+        //GREET
         sp = PreferenceManager.getDefaultSharedPreferences(getContext());
         int id =sp.getInt(LoginActivity.SEND_LOGIN, 0);
         userDB = new UserDB(getContext());
-
         User user = userDB.getUserDetail(id);
-        tvGreet.setText("Hello " + user.getUserName());
+        tvGreet.setText("Helloo " + user.getUserName());
+
+        //Click Card
+        cvFutsal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "futsal", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cvBasket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Basket", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cvBadminton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Badminton", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return view;
     }
