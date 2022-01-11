713e3f28dab9909a1418e98b94dd12604728d8f2
GazicAditya <gazictotti@gmail.com>
2022-01-11 17:16:35 +0700

    Fragment test

---
+++ app/src/main/java/com/example/mtiproject_olahra_go/RegisterVendor.java
@@ -0,0 +1,14 @@
+package com.example.mtiproject_olahra_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterVendor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_vendor);
+    }
+}
--- app/src/main/java/com/example/mtiproject_olahra_go/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/mtiproject_olahra_go/fragment/ProfileFragment.java
@@ -1,15 +1,34 @@
 package com.example.mtiproject_olahra_go.fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.mtiproject_olahra_go.LoginActivity;
 import com.example.mtiproject_olahra_go.R;
+import com.example.mtiproject_olahra_go.RegisterVendor;
+import com.example.mtiproject_olahra_go.User;
+import com.example.mtiproject_olahra_go.UserDB;
+
+import org.w3c.dom.Text;
+
 public class ProfileFragment extends Fragment {
+
+    SharedPreferences sp;
+    User user;
+    UserDB userDB;
+
+    TextView tvNamaUser, tvTelpUser, tvEmailUser, tvSignOut;
+    Button btnRegistVendor;
 
     public ProfileFragment() {
 
@@ -28,6 +47,39 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        int id = sp.getInt(LoginActivity.SEND_LOGIN, 0);
+        userDB = new UserDB(getContext());
+
+        user = userDB.getUserDetail(id);
+
+        tvNamaUser = view.findViewById(R.id.tvNamaUser);
+        tvTelpUser = view.findViewById(R.id.tvTelpUser);
+        tvEmailUser = view.findViewById(R.id.tvEmailUser);
+        tvSignOut = view.findViewById(R.id.tvSignOut);
+        btnRegistVendor = view.findViewById(R.id.btnRegistVendor);
+
+        tvNamaUser.setText(user.getUserName());
+        tvTelpUser.setText(user.getUserPhone());
+        tvEmailUser.setText(user.getUserEmail());
+
+        tvSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               getActivity().finish();
+            }
+        });
+
+        btnRegistVendor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), RegisterVendor.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
