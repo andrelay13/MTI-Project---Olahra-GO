f476432954db7454e29522a0e32b70b818651ed1
GazicAditya <gazictotti@gmail.com>
2022-01-11 15:23:35 +0700

    Fragment test

--- app/src/main/java/com/example/mtiproject_olahra_go/fragment/HomeFragment.java
+++ app/src/main/java/com/example/mtiproject_olahra_go/fragment/HomeFragment.java
@@ -3,17 +3,22 @@ package com.example.mtiproject_olahra_go.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mtiproject_olahra_go.R;
 
 public class HomeFragment extends Fragment {
-
+    
+    Button buttonTrigger;
+    
     public HomeFragment() {
-        // Required empty public constructor
+        
     }
 
     public static HomeFragment newInstance() {
@@ -29,6 +34,22 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        buttonTrigger = view.findViewById(R.id.buttonTrigger);
+        
+        buttonTrigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BookingFragment bookingFragment = BookingFragment.newInstance();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+                transaction.replace(R.id.fragments, bookingFragment);
+                transaction.commit();
+            }
+        });
+                
+        return view;
     }
 }
